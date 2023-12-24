181c5a92ba4b0f063fb7d0188e0ff5ca478a7893
fairoz <fairoz7204284@gmail.com>
2023-12-24 12:35:57 +0530

    fragments

---
+++ fragments/src/App.tsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Healthy Food</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Dal</li>
+        <li className="list-group-item">Green Vegitable</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </React.Fragment>
+  )
+}
+
+export default App
